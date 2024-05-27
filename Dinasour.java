1420ff3697f4ee37e24d492ef67369cba5564b1e
Gricelda Padilla <114915430+Gricelda20@users.noreply.github.com>
2024-05-27 12:59:46 -0400

    Initial commit

---
+++ Dinasour.java
@@ -0,0 +1,20 @@
+package com.coderscampus.oop;
+
+public class Dinasour {
+	String weight;
+	String length;	
+
+	String getinfo() {
+		return " Weight: " + weight + ", "
+				+ " Length: " + length;
+				
+	}
+	
+	void setProperties (int i, int j) {
+			
+	
+
+		
+	}
+ }
+
---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.coderscampus.oop;
+
+public class Main {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Dinasour wilbur = new Dinasour();
+		
+		wilbur.setProperties(800,100);
+		
+		System.out.println(wilbur.getinfo());
+	}
+
+}
+
